dc1d189a037774a67f8251f5c118763218e28cd0
astrocat879 <astrocat879@gmail.com>
2022-09-17 00:26:35 -0400

    setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCICPepb-VHI8gyp7lnExZ7LDXHHBzrC20",
+  authDomain: "wiz-cats.firebaseapp.com",
+  databaseURL: "https://wiz-cats-default-rtdb.firebaseio.com",
+  projectId: "wiz-cats",
+  storageBucket: "wiz-cats.appspot.com",
+  messagingSenderId: "160626562720",
+  appId: "1:160626562720:web:55a12ada7c9952e98e5bc5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
